76add2a3cf36de554f88fe6cb67becfa406e9c21
Zachary Rump <zrrm74@mail.missouri.edu>
2017-09-26 17:27:32 -0500

    Have [u,v,theta] for distinct objects.
    Need to look into Hough transform for line detection.
    and use Histogram for finding appropriate gray threshold.

--- lab2/part1/extract_orientations.m
+++ lab2/part1/extract_orientations.m
@@ -3,18 +3,29 @@
 % Output matrix nx3, n = number of objects
 
 % read image specified by 'filename'
-img = imread('left01.ppm');
+img = imread('right02.ppm');
 
 I = rgb2gray(img);
+
 % find grey threshold & binarize image 
-level = graythresh(I);
+%level = graythresh(I);    
+% IMPROVEMENT? => Use histogram to find gray threshold
+level = 0.95;
 bin = imbinarize(I,level);
 
 % which algorithm?
-BW = edge(bin, 'canny');
-
+%BW = edge(bin, 'Canny');
+% IMPROVEMENT? => Use utso method to find thresholding 
+threshold = 0.98;
+[BW,threshold] = edge(bin, 'Canny', threshold);
 % assign labels for objects
 L = bwlabel(BW);
 
 % Orientation is angle w.r.t. x-axis
 stats = regionprops(L, 'Centroid', 'Orientation');
+
+centroids = cat(1, stats.Centroid);
+imshow(BW)
+hold on 
+plot(centroids(:,1),centroids(:,2), 'b*')
+hold off
