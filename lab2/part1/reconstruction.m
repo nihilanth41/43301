1dba367e1dcdec8dcb9a5864de30443a5c570e79
Zachary Rump <zrrm74@mail.mizzou.edu>
2017-10-09 12:43:53 -0500

    Find error between reconstructed XYZ (M) and lab1

--- lab2/part1/reconstruction.m
+++ lab2/part1/reconstruction.m
@@ -1,7 +1,11 @@
 % Get P_left, P_right calibration matrix
+% also UV_{left,right} and XYZ_{left,right}
 load('projection.mat');
 % Get [u,v], theta for objects stats_left, stats_right
 load('stats.mat');
+
+% Right and left should be the same 
+XYZ = XYZ_left;
 
 % size(A) = 4,3
 % M = [x;y;z] 3x1
@@ -18,16 +22,8 @@ v_l = centroids_left(1,2);
 
 %M = reconstruct3d(u_l,v_l,u_r,v_r,P_left,P_right);
 
-load('calib_left.mat', 'UV', 'XYZ');
-UV_left = UV;
-XYZ_left = XYZ;
-
-load('calib_right.mat', 'UV', 'XYZ');
-UV_right = UV;
-XYZ_right = XYZ;
-
 % for each [u,v]_{left,right} compare result M to XYZ
-for i = 1:192
+for i = 1:length(XYZ)
     M = reconstruct3d(UV_left(1,i),UV_left(2,i),UV_right(1,i),UV_right(2,i),P_left,P_right);
     E(:,i) = XYZ(:,i) - M;
 end
