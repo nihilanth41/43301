e8a5f1b614830a191492008b0cb26749d61c21f3
Zachary Rump <zrrm74@mail.mizzou.edu>
2017-10-10 17:02:45 -0500

    Part 1 Part 1

---
+++ lab3/base2end.m
@@ -0,0 +1,42 @@
+function [ T ] = base2end( theta )
+%BASE2END Determines total transformation between base and end-effector.
+% INPUT: joint angles theta_1 .. theta_n  
+% OUTPUT: Transformation matrix T, 0A1*1A2*..*
+% Lecture 21 Page 2
+
+    % Numbers in inches ** need to convert to same units as used for projection
+    % matrix == mm? 
+    a2 = 8;
+    %a3 = -(5/16);
+    a3 = 0; % Our robots a3 = 0 from lect21.
+    d2 = (2+(7/8)) + (2+(1/16)); % Option 3
+    d3 = 0;                      % Option 3
+    d4 = 8;
+    d6 = 2.202;
+    A1 = [ cos(theta(1)) 0 -sin(theta(1)) 0;
+           sin(theta(1)) 0 cos(theta(1)) 0;
+           0 -1 -0 0;
+           0 0 0 1; ]; 
+    A2 = [ cos(theta(2)) -sin(theta(2)) 0 a2*cos(theta(2));
+           sin(theta(2)) cos(theta(2)) 0 a2*sin(theta(2));
+           0 0 1 d2;
+           0 0 0 1; ];
+    A3 = [ cos(theta(3)) 0 sin(theta(3)) a3*cos(theta(3));
+           sin(theta(3)) 0 cos(theta(3)) a3*sin(theta(3));
+           0 1 0 0;
+           0 0 0 1; ];
+    A4 = [ cos(theta(4)) 0 -sin(theta(4)) 0;
+           sin(theta(4)) 0 cos(theta(4)) 0;
+           0 -1 0 d4;
+           0 0 0 1; ];
+    A5 = [ cos(theta(5)) 0 sin(theta(5)) 0;
+           sin(theta(5)) 0 -cos(theta(5)) 0;
+           0 1 0 0;
+           0 0 0 1; ];
+    A6 = [ cos(theta(6)) -sin(theta(6)) 0 0;
+           sin(theta(6)) cos(theta(6)) 0 0;
+           0 0 1 d6;
+           0 0 0 1 ];
+    T = A1*A2*A3*A4*A5*A6;  
+end
+
---
+++ lab3/test_part1.m
@@ -0,0 +1,2 @@
+joint_angles = [ 30 30 30 30 30 30 ];
+T = base2end(joint_angles);
